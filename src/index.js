83088721dae030ac04a24a445b95f25f14daedd0
ashmdavis <68405151+ashmdavis@users.noreply.github.com>
2026-02-07 18:58:03 -0500

    started screen responsiveness

--- src/index.js
+++ src/index.js
@@ -4,26 +4,38 @@ import loadMenu from "./scripts/menu.js";
 import loadAbout from "./scripts/about.js";
 
 function setActiveButton(activeButton) {
-    const buttons = document.querySelectorAll("nav button");
+    const buttons = document.querySelectorAll("nav button, footer Button");
     buttons.forEach((button) => button.classList.remove("active"));
     if (activeButton) activeButton.classList.add("active");
 }
 
 loadHome();
 
+// NAV BUTTONS
 const homeButton = document.querySelector("#home");
 const menuButton = document.querySelector("#menu");
 const aboutButton = document.querySelector("#about");
 
+// FOOTER BUTTONS
+const footerHomeButton = document.querySelector("#footer-home");
+const footerMenuButton = document.querySelector("#footer-menu");
+const footerAboutButton = document.querySelector("#footer-about");
+
 setActiveButton(homeButton);
 
-function addListeners(button, callback) {
-    button.addEventListener("click", () => {
-        callback();
-        setActiveButton(button);
+function linkButtons(buttons, callback, mainButton) {
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            callback();
+            setActiveButton(mainButton);
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        });
     });
 }
 
-addListeners(homeButton, loadHome);
-addListeners(menuButton, loadMenu);
-addListeners(aboutButton, loadAbout);
+linkButtons([homeButton, footerHomeButton], loadHome, homeButton);
+linkButtons([menuButton, footerMenuButton], loadMenu, menuButton);
+linkButtons([aboutButton, footerAboutButton], loadAbout, aboutButton);
